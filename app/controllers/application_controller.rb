2250157b6eb89a10a10a5691c40c7eff9d6a932c
Nissan Holzer <nissan.holzer@gmail.com>
2019-05-12 16:43:23 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -42,7 +42,7 @@ class ApplicationController < Sinatra::Base
   
   patch '/articles/:id' do 
     @article = Article.find(params[:id])
-    @article.update = params[]
+    @article.update(params[:article])
     redirect "/articles/#{@article.id}"
   end
 
